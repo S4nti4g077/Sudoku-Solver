7103f0763dd6d3ce90d6e2c674ecc05bf4b185c2
Santiago <santiago.perez777@gmail.com>
2022-10-14 14:00:27 +0200

    working, no fancy look

--- source/app.js
+++ source/app.js
@@ -17,14 +17,15 @@ const solve = solveBtn.addEventListener("click", function () {
     if (el.value) {
       boxesArray.push(el.value);
     } else {
-      boxesArray.push("0");
+      boxesArray.push(".");
     }
   });
   console.log(boxesArray);
-  return boxesArray.join();
+  const solveSudoku = boxesArray.join("");
+  sudokuSolver(solveSudoku);
 });
 
-const sudokuSolver = async () => {
+const sudokuSolver = async (data) => {
   const options = {
     method: "POST",
     url: "https://solve-sudoku.p.rapidapi.com/",
@@ -33,11 +34,18 @@ const sudokuSolver = async () => {
       "X-RapidAPI-Key": "eb25f7b524mshc6ceba9410eefdcp159a3djsn6774bd1cbf53",
       "X-RapidAPI-Host": "solve-sudoku.p.rapidapi.com",
     },
-    body: `{"puzzle":"${solve}"}`,
+    body: `{"puzzle":"${data}"}`,
+  };
+
+  const populateValues = (solution) => {
+    const inputBoxes = document.querySelectorAll(".inputBox");
+    if (solution) {
+      inputBoxes.forEach((el, i) => (el.value = solution[i]));
+    }
   };
 
   fetch("https://solve-sudoku.p.rapidapi.com/", options)
     .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then((response) => populateValues(response.solution))
     .catch((err) => console.error(err));
 };
